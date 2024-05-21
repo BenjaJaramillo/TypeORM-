128eb9e734026bb43bc2ef57976e0705fa009b4d
Benjamin Jaramillo <benja.31jaramillo@gmail.com>
2024-05-20 20:42:16 -0600

    Con mysql

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 // Importar la entidad User desde el archivo "./entities/user".
 import { User } from "./entities/user";
 
-// Crear una nueva instancia de DataSource para la aplicación.
+//POSTGRES
 export const AppDataSource = new DataSource({
   // Configuración de la conexión a la base de datos PostgreSQL.
   type: "postgres", // Tipo de base de datos (PostgreSQL en este caso).
@@ -17,3 +17,19 @@ export const AppDataSource = new DataSource({
   logging: true, // Activar el registro de consultas SQL en la consola (solo para desarrollo).
   entities: [User], // Lista de entidades que serán gestionadas por TypeORM.
 });
+
+/*
+//MYSQL
+export const AppDataSource = new DataSource({
+  // Configuración de la conexión a la base de datos MySQL.
+  type: "mysql", // Tipo de base de datos (MySQL en este caso).
+  host: "localhost", // Dirección IP o nombre de dominio del servidor en la nube.
+  port: 3307, // Puerto de la base de datos (normalmente es 3306 para MySQL).
+  username: "admin", // Nombre de usuario para acceder a la base de datos en la nube.
+  password: "123", // Contraseña del usuario para acceder a la base de datos en la nube.
+  database: "crud", // Nombre de la base de datos en la nube a la que se conectará.
+  synchronize: false, // Desactivar la sincronización automática de las definiciones de las entidades con la base de datos en producción.
+  logging: true, // Activar el registro de consultas SQL en la consola (solo para desarrollo).
+  entities: [User], // Lista de entidades que serán gestionadas por TypeORM.
+});
+*/
